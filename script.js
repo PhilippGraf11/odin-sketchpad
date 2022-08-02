2434723857daedf93455946085a82ba8d1a7a049
Philipp <philippgepunkt@gmail.com>
2022-08-02 12:36:52 +0200

    alles fertig

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const cdiv = document.querySelector('.container');
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
+  // Add 16 divs
+  for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div');
+    
+    cdiv.appendChild(div);
+    div.setAttribute('id', 'farbe')
+  }
+  // when the button is clicked, the grid size is changed
+  const button = document.querySelector('.neues');
+  button.addEventListener('click', function() {
+    const cdiv = document.querySelector('.container');
+    cdiv.innerHTML = '';
+    const input = prompt('Enter a number between 1 and 256');
+    for (let i = 1; i < input; i++) {
+      const div = document.createElement('div');
+      cdiv.appendChild(div);
+      div.setAttribute('id', 'farbe')
+    }
+   
+    }
   
-  cdiv.appendChild(div);
-  div.setAttribute('id', 'farbe')
-}
+    );
 
 
 
