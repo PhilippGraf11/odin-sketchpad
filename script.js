b085255b8b752a6eb63e4c47f0edd8054f7f6c32
PhilippGraf11 <philippgepunkt@gmail.com>
2022-08-04 00:40:17 +0200

    nur noch das neue grid amk

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ const cdiv = document.querySelector('.container');
     cdiv.appendChild(div);
     div.setAttribute('id', 'farbe')
   }
-  // when the button is clicked, the grid size is changed
-  const button = document.querySelector('.neues');
-  button.addEventListener('click', function() {
-    const cdiv = document.querySelector('.container');
-    cdiv.innerHTML = '';
-    const input = prompt('Enter a number between 1 and 256');
-    for (let i = 1; i < input; i++) {
-      const div = document.createElement('div');
-      cdiv.appendChild(div);
-      div.setAttribute('id', 'farbe')
+
+const button = document.querySelector('.neues');
+button.addEventListener ('click',function(){
+  const gridsize = prompt ('Enter a number between 1 and 256');
+  if (gridsize > 0 && gridsize < 257) {
+
+    // Remove all divs
+    while (cdiv.firstChild) {
+      cdiv.removeChild(cdiv.firstChild);
     }
-   
-    }
+    // hier kommen die neuen divs rein und werden mit der Größe des Grids gesetzt
+    
+
+
+    
+  }
+  else {
+    alert ('Please enter a number between 1 and 256');
+  }
+
+
+}
+)
   
-    );
 
 
 
